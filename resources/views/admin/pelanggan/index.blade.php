77692ab44c85ecd89b61b3ca875aca996a6437e8
Neysa <muzian24ti@mahasiswa.pcr.ac.id>
2025-11-27 07:47:44 +0700

    rttt

--- resources/views/admin/pelanggan/index.blade.php
+++ resources/views/admin/pelanggan/index.blade.php
@@ -1,87 +1,58 @@
 @extends('admin.layout.app')
-@section('title', 'List Data Pelanggan')
+@section('title', 'List Data user')
 @section('content')
-    {{-- Content --}}
-    <div class="py-4">
-        <nav aria-label="breadcrumb" class="d-none d-md-inline-block">
-            <ol class="breadcrumb breadcrumb-dark breadcrumb-transparent">
-                <li class="breadcrumb-item">
-                    <a href="#">
-                        <svg class="icon icon-xxs" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
-                            </path>
-                        </svg>
-                    </a>
-                </li>
-                <li class="breadcrumb-item"><a href="{{ route('pelanggan.index') }}">Pelanggan</a></li>
-            </ol>
-        </nav>
-        <div class="d-flex justify-content-between w-100 flex-wrap">
-            <div class="mb-3 mb-lg-0">
-                <h1 class="h4">Data Pelanggan</h1>
-                <p class="mb-0">List data seluruh pelanggan</p>
-            </div>
-            <div>
-                <a href="{{ route('pelanggan.create') }}" class="btn btn-success text-white"><i
-                        class="far fa-question-circle me-1"></i> Tambah Pelanggan</a>
-            </div>
+{{-- Content --}}
+<div class="py-4">
+    <nav aria-label="breadcrumb" class="d-none d-md-inline-block">
+        <ol class="breadcrumb breadcrumb-dark breadcrumb-transparent">
+            <li class="breadcrumb-item">
+                <a href="#">
+                    <svg class="icon icon-xxs" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
+                        </path>
+                    </svg>
+                </a>
+            </li>
+            <li class="breadcrumb-item"><a href="{{ route('user.index') }}">user</a></li>
+        </ol>
+    </nav>
+    <div class="d-flex justify-content-between w-100 flex-wrap">
+        <div class="mb-3 mb-lg-0">
+            <h1 class="h4">Data user</h1>
+            <p class="mb-0">List data seluruh user</p>
+        </div>
+        <div>
+            <a href="{{ route('user.create') }}" class="btn btn-success text-white"><i
+                    class="far fa-question-circle me-1"></i> Tambah user</a>
         </div>
     </div>
+</div>
 
-    <div class="row">
-        <div class="col-12 mb-4">
-            <div class="card border-0 shadow mb-4">
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <form method="GET" action="{{ route('pelanggan.index') }}" onchange="this.form.submit()"
-                            class="mb-3">
-                            <div class="row">
-                                <div class="col-md-3">
-                                    <div class="input-group">
-                                        <input type="text" name="search" class="form-control" id="exampleInputIconRight"
-                                            value="{{ request('search') }}" placeholder="Search" aria-label="Search">
-                                        <button type="submit" class="input-group-text" id="basic-addon2">
-                                            <svg class="icon icon-xxs" fill="currentColor" viewBox="0 0 20 20"
-                                                xmlns="http://www.w3.org/2000/svg">
-                                                <path fill-rule="evenodd"
-                                                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                                    clip-rule="evenodd"></path>
-                                            </svg>
-                                        </button>
-                                    </div>
-                                </div>
-                        </form>
-
-                        <table>...</table>
-
-                        ...
-                    </div>
-                    <table id="table-pelanggan" class="table table-centered table-nowrap mb-0 rounded">
+<div class="row">
+    <div class="col-12 mb-4">
+        <div class="card border-0 shadow mb-4">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table id="table-user" class="table table-centered table-nowrap mb-0 rounded">
                         <thead class="thead-light">
                             <tr>
-                                <th>no</th>
-                                <th class="border-0">FirstName</th>
-                                <th class="border-0">LastName</th>
-                                <th class="border-0">Birthday</th>
-                                <th class="border-0">Gender</th>
+                                <th class="border-0">Name</th>
                                 <th class="border-0">Email</th>
-                                <th class="border-0">Phone</th>
+                                <th class="border-0">Password</th>
+                                <th class="border-0">Confirm password</th>
                                 <th class="border-0 rounded-end">Action</th>
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($dataPelanggan as $index => $item)
+                            @foreach ($dataUser as $item)
                                 <tr>
-                                    <td>{{ $dataPelanggan->firstItem() + $index }}</td>
-                                    <td>{{ $item->first_name }}</td>
-                                    <td>{{ $item->last_name }}</td>
-                                    <td>{{ $item->birthday }}</td>
-                                    <td>{{ $item->gender }}</td>
+                                    <td>{{ $item->name }}</td>
                                     <td>{{ $item->email }}</td>
-                                    <td>{{ $item->phone }}</td>
-                                    <td><a href="{{ route('pelanggan.edit', $item->pelanggan_id) }}"
+                                    <td>{{ $item->password }}</td>
+                                    <td>{{ $item->password_confirmation }}</td>
+                                    <td><a href="{{ route('user.edit', $item->id) }}"
                                             class="btn btn-info btn-sm ">
                                             <svg class="icon icon-xs me-2" data-slot="icon" fill="none"
                                                 stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24"
@@ -92,8 +63,8 @@
                                             </svg>
                                             Edit
                                         </a>
-                                        <form action="{{ route('pelanggan.destroy', $item->pelanggan_id) }}" method="POST"
-                                            style="display:inline">
+                                        <form action="{{ route('user.destroy', $item->id) }}"
+                                            method="POST" style="display:inline">
                                             @csrf
                                             @method('DELETE')
                                             <button type="submit" class="btn btn-danger btn-sm">
@@ -112,13 +83,9 @@
                             @endforeach
                         </tbody>
                     </table>
-                    <table id="table-pelanggan">...</table>
-                    <div class="mt-3">
-                        {{ $dataPelanggan->links('pagination::bootstrap-5') }}
-                    </div>
                 </div>
             </div>
         </div>
     </div>
-    </div>
+</div>
 @endsection
